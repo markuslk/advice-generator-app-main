5d83f741b2bf357465cff3054f99adbc2dfb4d03
Markus <markuslokk@gmail.com>
2022-07-26 12:43:14 +0300

    formatted js code / added styles to button

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const adviceText = document.querySelector(".advice");
 const adviceNumber = document.querySelector(".advice_number");
 const generate = document.querySelector(".generate");
 
-async function getAdvice() {
-	const response = await fetch("https://api.adviceslip.com/advice");
-	const data = await response.json();
+const getAdvice = async () => {
+	try {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json();
+		advice = data.slip.advice;
+		adviceNum = data.slip.id;
 
-	advice = data.slip.advice;
-	adviceNum = data.slip.id;
-
-	adviceNumber.textContent = `advice #${adviceNum}`;
-	adviceText.textContent = `"${advice}"`;
-}
+		adviceNumber.textContent = `advice #${adviceNum}`;
+		adviceText.textContent = `"${advice}"`;
+	} catch (e) {
+		console.log("Error", e);
+	}
+};
 
 getAdvice();
 
